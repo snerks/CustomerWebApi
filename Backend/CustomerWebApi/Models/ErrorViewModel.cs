f9f7b80f7a6e65c7fb713f60182112441881f982
snerks <martin.github@outlook.com>
2017-10-09 15:42:26 +0100

    Initial commit

---
+++ Backend/CustomerWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomerWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
